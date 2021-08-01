8a68d0cb4ed099e1487735791ec8d29cc8c5de96
七画一只妖 <84392009+Twip-Emma@users.noreply.github.com>
2021-08-01 10:55:15 +0800

    Update AdminDaoImpl.java

--- Spring-Case/src/com/twip/bookstore/dao/AdminDaoImpl.java
+++ Spring-Case/src/com/twip/bookstore/dao/AdminDaoImpl.java
@@ -28,21 +28,31 @@ public class AdminDaoImpl implements AdminDao {
     //删除书
     @Override
     public Boolean deleteBook(Book book) {
-        String sql = "delete from book_store_ware where book_id=?";
-        Object[] args = {book.getBookId()};
-        int insert = jdbcTemplate.update(sql, args);
-        System.out.println("删除成功,影响行数：" + insert);
-        return true;
+        boolean re = book.isBookFlag();
+        if(!re){
+            return false;
+        }else{
+            String sql = "delete from book_store_ware where book_name=?";
+            Object[] args = {book.getBookName()};
+            int insert = jdbcTemplate.update(sql, args);
+            System.out.println("删除成功,影响行数：" + insert);
+            return true;
+        }
     }
 
     //批量添加相同书
     @Override
-    public Boolean insertBookBatch(Book book, int amount) {
-        String sql = "update book_store_ware set book_amount=book_amount+? where book_id=?";
-        Object[] args = {book.getBookAmount(), amount};
-        int update = jdbcTemplate.update(sql, args);
-        System.out.println("添加成功,影响行数：" + update);
-        return true;
+    public Boolean insertBookBatch(Book book) {
+        boolean re = book.isBookFlag();
+        if(!re){
+            return false;
+        }else{
+            String sql = "update book_store_ware set book_amount=book_amount+? where book_name=?";
+            Object[] args = {book.getBookAmount(), book.getBookName()};
+            int update = jdbcTemplate.update(sql, args);
+            System.out.println("添加成功,影响行数：" + update);
+            return true;
+        }
     }
 
     //操作前判断
