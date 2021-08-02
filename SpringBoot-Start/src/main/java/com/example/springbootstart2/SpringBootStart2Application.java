ec0a0383d8f2deec830260b25c3f22a770ef8f0d
七画一只妖 <84392009+Twip-Emma@users.noreply.github.com>
2021-08-02 20:48:05 +0800

    Add files via upload

---
+++ SpringBoot-Start/src/main/java/com/example/springbootstart2/SpringBootStart2Application.java
@@ -0,0 +1,11 @@
+package com.example.springbootstart2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStart2Application {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStart2Application.class, args);
+    }
+}
---
+++ SpringBoot-Start/src/test/java/com/example/springbootstart2/SpringBootStart2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootstart2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStart2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
