94260e3b9910c01a7052799867bbfb4fe233c4ae
七画一只妖 <84392009+Twip-Emma@users.noreply.github.com>
2021-08-17 16:04:06 +0800

    Update BookDao.java

--- SpringBoot-CRUD/src/main/java/com/example/springbootcase1/dao/BookDao.java
+++ SpringBoot-CRUD/src/main/java/com/example/springbootcase1/dao/BookDao.java
@@ -22,7 +22,14 @@ public interface BookDao {
             "book_store_ware(book_id,book_name,book_price,book_amount)" +
             "value(#{bookId},#{bookName},#{bookPrice},#{bookAmount})")
     void insertNewBook(Book book);
-//    @Options(useGeneratedKeys = true, keyColumn = "id", keyProperty = "id")
 
+    @Delete("delete from book_store_ware where book_id=#{id}")
+    Boolean deleteBook(String id);
 
+    //测试用的方法
+    @Delete("delete from book_store_ware where book_id=#{id}")
+    Boolean deleteBookReturnBool(String id);
+
+    @Update("update book_store_ware set book_amount=book_amount+#{bookAmount} where book_name=#{bookName}")
+    Boolean moreBook(String bookName,Integer bookAmount);
 }
