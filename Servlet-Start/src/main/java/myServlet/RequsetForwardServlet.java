61a1165d008f8e5b993a8e3fa7faa2e300344dbe
七画一只妖 <84392009+Twip-Emma@users.noreply.github.com>
2021-09-22 23:25:54 +0800

    Add files via upload

---
+++ Servlet-Start/src/main/java/myServlet/RequsetForwardServlet.java
@@ -0,0 +1,26 @@
+package main.java.myServlet;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class RequsetForwardServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doGet(req, resp);
+        resp.setContentType("text/html;charset=utf-8");
+        req.setCharacterEncoding("utf-8");
+        req.setAttribute("city","湘潭");
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/forwardTest");
+        dispatcher.forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doPost(req, resp);
+        doGet(req, resp);
+    }
+}
---
+++ Servlet-Start/src/main/java/myServlet/ResultServlet.java
@@ -0,0 +1,28 @@
+package main.java.myServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ResultServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doGet(req, resp);
+        resp.setContentType("text/html;charset=utf-8");
+        req.setCharacterEncoding("utf-8");
+        PrintWriter out = resp.getWriter();
+        String city = (String) req.getAttribute("city");
+        if(city != null){
+            out.print("你的城市名称："+ city);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doPost(req, resp);
+        doGet(req,resp);
+    }
+}
