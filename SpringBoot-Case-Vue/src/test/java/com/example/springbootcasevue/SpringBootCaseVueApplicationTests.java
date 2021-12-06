0afc55f5c39dd190e43f67866925c47dd75e40f7
七画一只妖 <1157529280@qq.com>
2021-12-06 11:36:27 +0800

    cookies练习

--- SpringBoot-Case-Vue/src/test/java/com/example/springbootcasevue/SpringBootCaseVueApplicationTests.java
+++ SpringBoot-Case-Vue/src/test/java/com/example/springbootcasevue/SpringBootCaseVueApplicationTests.java
@@ -1,13 +1,19 @@
 package com.example.springbootcasevue;
 
+import com.example.springbootcasevue.entity.User;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBootCaseVueApplicationTests {
 
+    @Autowired
+    User user;
+
     @Test
     void contextLoads() {
+        System.out.println(user.getA());
     }
 
 }
